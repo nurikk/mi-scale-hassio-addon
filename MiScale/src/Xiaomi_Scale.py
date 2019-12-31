161be3a26836f6f09f1dc7a4f6b6e86d4c08edad
fantomnotabene <fantomnotabene@gmail.com>
2019-12-31 10:40:38 +0300

    test

--- MiScale/src/Xiaomi_Scale.py
+++ MiScale/src/Xiaomi_Scale.py
@@ -1,37 +1,17 @@
-#############################################################################################
-# Code to read weight measurements fom Xiaomi Scale V2
-# (Framework is prsent to also read from Xiaomi Scale V1, though I do not own one to test so code has not been maintained)
-# Must be executed with Python 3 else body measurements are incorrect.
-# Must be executed as root, therefore best to schedule via crontab every 5 min (so as not to drain the battery):
-# */5 * * * * python3 /path-to-script/Xiaomi_Scale.py
-# Multi user possible as long as weitghs do not overlap, see lines 117-131
-#
-# Thanks to @syssi (https://gist.github.com/syssi/4108a54877406dc231d95514e538bde9) and @prototux (https://github.com/wiecosystem/Bluetooth) for their initial code
-#
-# Make sure you set your MQTT credentials below and user logic/data through the environment variables
-#
-#############################################################################################
-
-
-
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-from __future__ import print_function
-import argparse
-import binascii
-import time
+# coding: utf-8
+
 import os
+import time
 import json
-import sys
-from bluepy import btle
 import paho.mqtt.client as mqtt
+import Xiaomi_Scale_Body_Metrics as XSBM
+
+from bluepy import btle
 from datetime import datetime
 
-import Xiaomi_Scale_Body_Metrics
+CONFIG = json.load(open('/users/fantom/downloads/config.json'))
 
-CONFIG = json.load(open('/data/config.json'))
-
-# Configuraiton...
 MISCALE_MAC = CONFIG.get('miscale_mac', '')
 MQTT_USERNAME = CONFIG.get('mqtt_username', '')
 MQTT_PASSWORD = CONFIG.get('mqtt_password', '')
@@ -40,29 +20,12 @@ MQTT_PORT = CONFIG.get('mqtt_port', 1883)
 MQTT_TIMEOUT = CONFIG.get('mqtt_timeout', 60)
 MQTT_PREFIX = CONFIG.get('mqtt_prefix', 'miScale')
 
-# User Variables...
-
-USER1_GT = int(os.getenv('USER1_GT', '70')) # If the weight is greater than this number, we'll assume that we're weighing User #1
-USER1_SEX = os.getenv('USER1_SEX', 'male')
-USER1_NAME = os.getenv('USER1_NAME', 'David') # Name of the user
-USER1_HEIGHT = int(os.getenv('USER1_HEIGHT', '175')) # Height (in cm) of the user
-USER1_DOB = os.getenv('USER1_DOB', '1988-09-30') # DOB (in yyyy-mm-dd format)
-
-# USER2_LT = int(os.getenv('USER2_LT', '55')) # If the weight is less than this number, we'll assume that we're weighing User #2
-# USER2_SEX = os.getenv('USER2_SEX', 'female')
-# USER2_NAME = os.getenv('USER2_NAME', 'Joanne') # Name of the user
-# USER2_HEIGHT = int(os.getenv('USER2_HEIGHT', '155')) # Height (in cm) of the user
-# USER2_DOB = os.getenv('USER2_DOB', '1988-10-20') # DOB (in yyyy-mm-dd format)
-
-# USER3_SEX = os.getenv('USER3_SEX', 'male')
-# USER3_NAME = os.getenv('USER3_NAME', 'Unknown User') # Name of the user
-# USER3_HEIGHT = int(os.getenv('USER3_HEIGHT', '175')) # Height (in cm) of the user
-# USER3_DOB = os.getenv('USER3_DOB', '1988-01-01') # DOB (in yyyy-mm-dd format)
-
+USERS = CONFIG.get('users', [])
 
 class ScanProcessor():
 
-	def GetAge(self, d1):
+	def getAge(self, d1):
+		if not d1: return
 		d1 = datetime.strptime(d1, "%Y-%m-%d")
 		d2 = datetime.strptime(datetime.today().strftime('%Y-%m-%d'),'%Y-%m-%d')
 		return abs((d2 - d1).days)/365
@@ -74,13 +37,6 @@ class ScanProcessor():
 
 	def handleDiscovery(self, dev, isNewDev, isNewData):
 		if dev.addr == MISCALE_MAC.lower() and isNewDev:
-			# print ('	Device: %s (%s), %d dBm %s. ' %
-				   # (
-					   # ANSI_WHITE + dev.addr + ANSI_OFF,
-					   # dev.addrType,
-					   # dev.rssi,
-					   # ('' if dev.connectable else '(not connectable)'))
-				   # , end='')
 			for (sdid, _, data) in dev.getScanData():
 				### Xiaomi V1 Scale ###
 				if data.startswith('1d18') and sdid == 22:
@@ -109,8 +65,6 @@ class ScanProcessor():
 					mitdatetime = datetime.strptime(str(int((data[10:12] + data[8:10]), 16)) + " " + str(int((data[12:14]), 16)) +" "+ str(int((data[14:16]), 16)) +" "+ str(int((data[16:18]), 16)) +" "+ str(int((data[18:20]), 16)) +" "+ str(int((data[20:22]), 16)), "%Y %m %d %H %M %S")
 					miimpedance = str(int((data[24:26] + data[22:24]), 16))
 
-
-
 					if unit:
 						print('')
 						self._publish(round(measured, 2), unit, str(mitdatetime), miimpedance)
@@ -128,7 +82,6 @@ class ScanProcessor():
 
 		def _on_connect(client, _, flags, return_code):
 			self.connected = True
-			#print("MQTT connection returned result: %s" % mqtt.connack_string(return_code))
 
 		self.mqtt_client.on_connect = _on_connect
 
@@ -138,49 +91,46 @@ class ScanProcessor():
 	def _publish(self, weight, unit, mitdatetime, miimpedance):
 		if not self.connected:
 			raise Exception('not connected to MQTT server')
-		if int(weight) > USER1_GT:
-			user = USER1_NAME
-			height = USER1_HEIGHT
-			age = self.GetAge(USER1_DOB)
-			sex = USER1_SEX
-		# elif int(weight) < USER2_LT:
-		# 	user = USER2_NAME
-		# 	height = USER2_HEIGHT
-		# 	age = self.GetAge(USER2_DOB)
-		# 	sex = USER2_SEX
-		# else:
-		# 	user = USER3_NAME
-		# 	height = USER3_HEIGHT
-		# 	age = self.GetAge(USER3_DOB)
-		# 	sex = USER3_SEX
-		lib = Xiaomi_Scale_Body_Metrics.bodyMetrics(weight, height, age, sex, 0)
+		unknown = {
+			'name': 'Unknown',
+			'height': None,
+			'birthdate': None,
+			'sex': None
+		}
+		check_user = lambda u, w: u['weight_greater_than'] < w < u['weight_lower_than']
+		user = next((u for u in USERS if check_user(u, weight)), unknown)
+		user = user['name']
+		height = user['height']
+		age = self.getAge(user['birthdate'])
+		sex = 'male' if user['sex'] else 'female'
+		# lib = XSBM.bodyMetrics(weight, height, age, sex, 0)
 		message = '{'
 		message += '"Weight":"' + "{:.2f}".format(weight) + '"'
-		message += ',"BMI":"' + "{:.2f}".format(lib.getBMI()) + '"'
-		message += ',"Basal Metabolism":"' + "{:.2f}".format(lib.getBMR()) + '"'
-		message += ',"Visceral Fat":"' + "{:.2f}".format(lib.getVisceralFat()) + '"'
+		# message += ',"BMI":"' + "{:.2f}".format(lib.getBMI()) + '"'
+		# message += ',"Basal Metabolism":"' + "{:.2f}".format(lib.getBMR()) + '"'
+		# message += ',"Visceral Fat":"' + "{:.2f}".format(lib.getVisceralFat()) + '"'
 
-		if miimpedance:
-			lib = Xiaomi_Scale_Body_Metrics.bodyMetrics(weight, height, age, sex, int(miimpedance))
-			message += ',"Lean Body Mass":"' + "{:.2f}".format(lib.getLBMCoefficient()) + '"'
-			message += ',"Body Fat":"' + "{:.2f}".format(lib.getFatPercentage()) + '"'
-			message += ',"Water":"' + "{:.2f}".format(lib.getWaterPercentage()) + '"'
-			message += ',"Bone Mass":"' + "{:.2f}".format(lib.getBoneMass()) + '"'
-			message += ',"Muscle Mass":"' + "{:.2f}".format(lib.getMuscleMass()) + '"'
-			message += ',"Protein":"' + "{:.2f}".format(lib.getProteinPercentage()) + '"'
-			self.mqtt_client.publish(MQTT_PREFIX + '/' + user, weight, qos=1, retain=True)
+		# if miimpedance:
+		# 	lib = XSBM.bodyMetrics(weight, height, age, sex, int(miimpedance))
+			# message += ',"Lean Body Mass":"' + "{:.2f}".format(lib.getLBMCoefficient()) + '"'
+			# message += ',"Body Fat":"' + "{:.2f}".format(lib.getFatPercentage()) + '"'
+			# message += ',"Water":"' + "{:.2f}".format(lib.getWaterPercentage()) + '"'
+			# message += ',"Bone Mass":"' + "{:.2f}".format(lib.getBoneMass()) + '"'
+			# message += ',"Muscle Mass":"' + "{:.2f}".format(lib.getMuscleMass()) + '"'
+			# message += ',"Protein":"' + "{:.2f}".format(lib.getProteinPercentage()) + '"'
+			# self.mqtt_client.publish(MQTT_PREFIX + '/' + user, weight, qos=1, retain=True)
 
-		message += ',"TimeStamp":"' + mitdatetime + '"'
+		# message += ',"TimeStamp":"' + mitdatetime + '"'
 		message += '}'
 		self.mqtt_client.publish(MQTT_PREFIX + '/' + user + '/weight', message, qos=1, retain=True)
 		print('\tSent data to topic %s: %s' % (MQTT_PREFIX + '/' + user + '/weight', message))
 
 def main():
-
-	# while(True):
 	scanner = btle.Scanner().withDelegate(ScanProcessor())
-
 	_ = scanner.scan(5)
 
-if __name__ == "__main__":
-	main()
+if __name__ == '__main__':
+	try:
+		main()
+	except Exception as e:
+		print(e)
